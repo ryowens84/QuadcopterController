0b51f75bc2b89dcb0f70552d6f8c6f3ed79d6609
Ryan Owens <ryan@sparkfun.com>
2011-04-15 20:07:27 -0600

    Added macros for COMM_ON and COMM_OFF

--- Main/main.h
+++ Main/main.h
@@ -3,11 +3,15 @@
 
 #define LED	(1<<15)
 #define XBEE_EN	(1<<10)
+#define COMM	(1<<12)
 
 #define LED_ON()		IOSET0 = LED
 #define LED_OFF()		IOCLR0 = LED
 
 #define XBEEon()		IOSET0 = XBEE_EN
 #define XBEEoff()		IOCLR0 = XBEE_EN
+
+#define COMM_ON()		IOSET0 = COMM
+#define COMM_OFF()		IOCLR0 = COMM
 
 #endif
